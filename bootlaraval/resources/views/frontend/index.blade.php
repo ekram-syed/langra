86a2cb342dbac0678bf0bf31f8fe063e75e0d6c6
Syed Ekram Uddin Emon <ekram.syed@gmail.com>
2018-03-09 18:17:16 +0600

    App frontend integration done

--- bootlaraval/resources/views/frontend/index.blade.php
+++ bootlaraval/resources/views/frontend/index.blade.php
@@ -1,1 +1,187 @@
-@extends('layouts.frontend')
+@extends('layouts.frontend') @section('content')
+<section id="hero" class="jumbotron text-center rounded-0 mb-0">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 align-middle text-left">
+                <h2 class="jumbotron-heading">
+                    The convenient and fast way to get things done with the LIVE touch of Experts
+                </h2>
+                <p class="lead text-muted my-5">
+                    Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too
+                    short so folks don't simply skip over it entirely. Getting a quick job done has never been easier
+                </p>
+                <p class="my-5">
+                    <a href="#" class="btn btn-outline-primary my-2">
+                        <i class="fas fa-user-plus"></i> Get Stated</a>
+                    <a href="#" class="btn btn-outline-info my-2">
+                        <i class="fas fa-book"></i> Learn More</a>
+                    <a href="#" class="btn btn-outline-success my-2">
+                        <i class="fas fa-graduation-cap"></i> Im an Expert</a>
+                </p>
+            </div>
+            <div class="col">
+                <img src="img/jumbotron.png" class="img-fluid w-75" alt="A generic square placeholder image with rounded corners in a figure.">
+            </div>
+        </div>
+</section>
+
+
+<section id="featured-categories">
+    <div class="container py-5 border-top text-center">
+        <h2>Find someone perfect for your needs</h2>
+        <div class="input-group my-3">
+            <input type="text" class="form-control border-secondary" placeholder="What do you need to get done?" aria-label="Recipient's username"
+                aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                    <i class="fas fa-search"></i> Search</button>
+            </div>
+        </div>
+        <h3 class="text-muted">Whats popular</h3>
+        <div class="card-columns">
+            <a href="#" class="card  list-group-item-action border-light ">
+                <div class="card-body">
+                    <h5 class="card-title"> Guitar</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">5 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/guitar.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light">
+                <div class="card-body">
+                    <h5 class="card-title"> Chainies</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">5 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/translator.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light ">
+                <div class="card-body">
+                    <h5 class="card-title"> Math</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">5 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/maths.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light ">
+                <div class="card-body">
+                    <h5 class="card-title"> Yoga</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">5 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/yoga.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light">
+                <div class="card-body">
+                    <h5 class="card-title"> Cooking</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">5 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/chef.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light">
+                <div class="card-body">
+                    <h5 class="card-title"> Piano</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">5 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/piano.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light">
+                <div class="card-body">
+                    <h5 class="card-title"> Singing</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">95 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/microphone.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light">
+                <div class="card-body">
+                    <h5 class="card-title"> Drawing</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">45 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/pencil.png">
+                    </p>
+                </div>
+            </a>
+            <a href="#" class="card  list-group-item-action border-light">
+                <div class="card-body">
+                    <h5 class="card-title"> Care</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">500 Experts</h6>
+                    <p class="card-text">
+                        <img src="img/care.png">
+                    </p>
+                </div>
+            </a>
+        </div>
+
+        <a href="#" class="btn btn-secondary my-2">
+            <i class="fab fa-wpexplorer"></i> Explore More</a>
+    </div>
+</section>
+
+<section id="how-it-works">
+    <div class="container py-5 border-top text-center">
+        <h2 class="mb-5">How it works</h2>
+        <div class="row">
+            <div class="col-sm-4 col-lg-3 col-lg-offset-0 col-sm-offset-1">
+                <article class="text-block">
+                    <header>
+                        <img src="img/spy.png" alt="">
+                        <hr>
+                    </header>
+                    <h3 data-qa="name" class="text-uppercase m-b-40">
+                        FIND
+                    </h3>
+                    <p data-qa="details" class="lead">Browse profiles, reviews, and proposals to find the expert best match for your topic.</p>
+                </article>
+            </div>
+            <div class="m-b-80 col-sm-4 col-lg-3 col-lg-offset-0 col-sm-offset-2">
+                <article class="text-block">
+                    <header>
+                        <img src="img/human-resources.png" class="hiw-icon" alt="">
+                        <hr>
+                    </header>
+                    <h3 data-qa="name" class="text-uppercase m-b-40">
+                        HIRE
+                    </h3>
+                    <p data-qa="details" class="lead">Contact, Schedule, Talk and Hire the favorite.</p>
+                </article>
+            </div>
+            <div class="clearfix visible-sm visible-md"></div>
+            <div class="m-b-80 col-sm-4 col-lg-3 col-lg-offset-0 col-sm-offset-1">
+                <article class="text-block">
+                    <header>
+                        <img src="img/online.png" class="hiw-icon" alt="">
+                        <hr>
+                    </header>
+                    <h3 data-qa="name" class="text-uppercase m-b-40">
+                        Learn
+                    </h3>
+                    <p data-qa="details" class="lead">Start private live sessions with your provider, ask questions, be awesome</p>
+                </article>
+            </div>
+            <div class="m-b-80 col-sm-4 col-lg-3 col-lg-offset-0 col-sm-offset-2">
+                <article class="text-block">
+                    <header>
+                        <img src="img/handshake.png" class="hiw-icon" alt="">
+                        <hr>
+                    </header>
+                    <h3 data-qa="name" class="text-uppercase m-b-40">
+                        PAY
+                    </h3>
+                    <p data-qa="details" class="lead">Pay with our secured platform, be happy.</p>
+                </article>
+            </div>
+            <div class="clearfix visible-sm visible-md"></div>
+        </div>
+    </div>
+</section>
+@endsection
--- bootlaraval/resources/views/layouts/frontend.blade.php
+++ bootlaraval/resources/views/layouts/frontend.blade.php
@@ -1,100 +1,163 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">      
-        <title>{{ config('app.name', 'Laravel') }}</title>
-      
-        <!-- Favicons && Device Icons -->
-        <link rel="apple-touch-icon" sizes="57x57" href="ico/apple-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="ico/apple-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="ico/apple-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="ico/apple-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="ico/apple-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="ico/apple-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="ico/apple-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="ico/apple-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="ico/apple-icon-180x180.png">
-        <link rel="icon" type="image/png" sizes="192x192" href="ico/android-icon-192x192.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="ico/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="96x96" href="ico/favicon-96x96.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="ico/favicon-16x16.png">
-        <link rel="manifest" href="ico/manifest.json">
-        <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="ico/ms-icon-144x144.png">
-        <meta name="theme-color" content="#ffffff">
-      
-        <!-- CSS -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-      </head>
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Favicons && Device Icons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="ico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="ico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="ico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="ico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="ico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="ico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="ico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="ico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="ico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="ico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="ico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="ico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="ico/favicon-16x16.png">
+    <link rel="manifest" href="ico/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="ico/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
 <body>
-    <header id="header"  class="pt-5 mt-2">
-    <nav class="navbar navbar-expand-md fixed-top navbar-light border-bottom">
-      <div class="container">
-        <a class="navbar-brand " href="{{ url('/') }}">
-          <i class="fas fa-leaf text-success"></i> {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleContent" aria-controls="navbarSupportedContent"
-          aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <header id="header" class="pt-5 mt-2">
+        <nav class="navbar navbar-expand-md bg-bright navbar-laravel fixed-top navbar-light border-bottom">
+            <div class="container">
+                <a class="navbar-brand " href="{{ url('/') }}">
+                    <i class="fas fa-leaf text-success"></i> {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarToggleContent">
-          <ul class="navbar-nav">
-              <!-- Authentication Links -->
-              @guest
-              <li><a class="nav-link btn btn-outline-success mx-1" href="{{ route('login') }}"><i class="fas fa-user-plus"></i>{{ __('Signin') }}</a></li>
-              <li><a class="nav-link btn btn-outline-primary" href="{{ route('register') }}"><i class="fas fa-sign-in-alt"></i>{{ __('Signup') }}</a></li>
-          @else
-              <li class="nav-item dropdown">
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                      {{ Auth::user()->name }} <span class="caret"></span>
-                  </a>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarToggleContent">
+                    <ul class="navbar-nav">
+                        <!-- Authentication Links -->
+                        @guest
+                        <li>
+                            <a class=" btn btn-outline-success mx-1" href="{{ route('login') }}">
+                                <i class="fas fa-user-plus"></i> {{ __('Signin') }}</a>
+                        </li>
+                        <li>
+                            <a class=" btn btn-outline-primary" href="{{ route('register') }}">
+                                <i class="fas fa-sign-in-alt"></i> {{ __('Signup') }}</a>
+                        </li>
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class=" dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
+                                v-pre>
+                                {{ Auth::user()->name }}
+                                <span class="caret"></span>
+                            </a>
 
-                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <a class="dropdown-item" href="{{ route('logout') }}"
-                         onclick="event.preventDefault();
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                        document.getElementById('logout-form').submit();">
-                                       <i class="fas fa-sign-out-alt"></i>{{ __('Logout') }}
-                      </a>
+                                    <i class="fas fa-sign-out-alt"></i>{{ __('Logout') }}
+                                </a>
 
-                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                          @csrf
-                      </form>
-                  </div>
-              </li>
-          @endguest
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
-    
-    <main class="py-4">
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
         @yield('content')
     </main>
 
 
     <footer id="footer">
-            <div class="container border-top py-3 mt-5">
-              <div class="row justify-content-between">
+        <div class="container border-top py-3 mt-5">
+            <div class="row justify-content-between">
                 <div class="col-md-3">
-                  <a class="navbar-brand " href="#">
-                    <i class="fas fa-leaf text-muted"></i> <span class="text-muted">{{ config('app.name', 'Laravel') }}</span>
-                  </a>
+                    <a class="navbar-brand " href="#">
+                        <i class="fas fa-leaf text-muted"></i>
+                        <span class="text-muted">{{ config('app.name', 'Laravel') }}</span>
+                    </a>
+                    <small class="blockquote-footer">&copy; 2018 MetaActs Inc.</small>
                 </div>
-                <div class="col-md-3 text-right align-self-center">
-                  <small class="blockquote-footer">&copy; 2018 MetaActs Inc.</small>
+                <div class="col-md-3">
+                    <h6>Company</h6>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="about.html">About</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Terms of Service</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Trust &amp; Safety</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Careers</a>
+                        </li>
+                    </ul>
                 </div>
-              </div>
+                <div class="col-md-3">
+                    <h6>Support</h6>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="about.html">Contact Support</a>
+                        </li>
+                        <li>
+                            <a href="about.html">User Handbook</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Provider Handbook</a>
+                        </li>
+                        <li>
+                            <a href="about.html">How to Guide</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Blog</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Forum</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-3">
+                    <h6>Explore</h6>
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="about.html">Experts by Skill</a>
+                        </li>
+                        <li>
+                            <a href="about.html">Topics</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </footer>
+        </div>
+    </footer>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
